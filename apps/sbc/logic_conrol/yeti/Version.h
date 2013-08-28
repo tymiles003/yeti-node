b149f428d2ff5222fd20c5607d73f088eebf6200
murfur <void@onat.edu.ua>
2013-08-28 13:36:12 +0000

    rm Version.h

--- apps/sbc/logic_conrol/yeti/Version.h
+++
@@ -1,8 +0,0 @@
-#ifndef _Version_h_
-#define _Version_h_
-
-#define YETI_VERSION "9607bfbc02ba8f560901bfdd06f47ebe71f84515"
-#define YETI_BUILD_DATE "Wed Aug 28 12:56:43 EEST 2013"
-#define YETI_BUILD_USER "laeda"
-
-#endif
