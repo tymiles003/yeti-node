c9273976e16242fb70fc5c266709bfb6931c046b
murfur <void@onat.edu.ua>
2013-10-02 12:18:21 +0300

    change default config for xmlrpcdi plugin

--- apps/xmlrpc2di/etc/xmlrpc2di.conf.cmake
+++ apps/xmlrpc2di/etc/xmlrpc2di.conf.cmake
@@ -1,6 +1,6 @@
 # server_ip : IP to bind XMLRPC server to
 # leave empty for ANY interface
-#server_ip=127.0.0.1
+server_ip=127.0.0.1
 
 # port to bind XMLRPC server to 
 xmlrpc_port=8090
@@ -8,16 +8,16 @@ xmlrpc_port=8090
 # run multi-threaded server? 
 # Default: yes
 # 
-# multithreaded = yes
+multithreaded = yes
 
 # threads to run - this many requests can be processed in parallel
 # Default: 5
 #
-# threads=5
+threads=5
 
 # export all DI functions with the function call 'di'?
 # defaults to: yes
-# export_di=yes
+export_di=yes
 
 #
 # these DI interfaces are searched for functions to 
@@ -28,14 +28,14 @@ direct_export=sbc
 
 # run the XMLRPC server at all (default: yes)
 #
-# run_server=yes 
+run_server=yes 
 
 # timeout for client requests, in milliseconds (0 to disable)
 #
-# server_timeout=500
+server_timeout=500
 
 # print parameters of XMLRPC server calls into debug log [yes|no]
-#  debug_server_params=yes
+debug_server_params=yes
 #
 # print result of XMLRPC server calls into debug log [yes|no]
 #  debug_server_result=yes
