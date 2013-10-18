e07c6039bb9e27525556109487935272fa675818
murfur <void@onat.edu.ua>
2013-10-18 19:17:18 +0300

    rm Version.h

--- apps/sbc/logic_control/yeti/Version.h
+++
@@ -1,8 +0,0 @@
-#ifndef _Version_h_
-#define _Version_h_
-
-#define YETI_VERSION "fc83d273e0c601473167e231130081b85c77f790"
-#define YETI_BUILD_DATE "Fri Oct 18 14:04:43 EEST 2013"
-#define YETI_BUILD_USER "laeda"
-
-#endif
