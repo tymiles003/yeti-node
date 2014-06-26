11e30c8766162c7553f3178612754e0345cd75f1
murfur <void@onat.edu.ua>
2014-06-26 17:24:36 +0300

    AmSdp: comment code with session level removing

--- core/AmSdp.cpp
+++ core/AmSdp.cpp
@@ -316,14 +316,14 @@ void AmSdp::print(string& body) const
     "o="+origin.user+" "+int2str(origin.sessId)+" "+
     int2str(origin.sessV)+" IN ";
 
-  bool conn_in_all_media_streams = true;
+  /*bool conn_in_all_media_streams = true;
   for(std::vector<SdpMedia>::const_iterator media_it = media.begin();
 	  media_it != media.end(); media_it++) {
 		if(media_it->conn.address.empty()){
 			conn_in_all_media_streams = false;
 			break;
 		}
-  }
+  }*/
 
   if (!origin.conn.address.empty())
     if (origin.conn.address.find(".") != std::string::npos)
@@ -346,7 +346,7 @@ void AmSdp::print(string& body) const
   out_buf +=
     "s="+sessionName+"\r\n";
 
-if(!conn_in_all_media_streams) { //don't add to session level if present for all media streams
+//if(!conn_in_all_media_streams) { //don't add to session level if present for all media streams
   if (!conn.address.empty()) {
     if (conn.address.find(".") != std::string::npos)
       out_buf += "c=IN IP4 ";
@@ -354,7 +354,7 @@ if(!conn_in_all_media_streams) { //don't add to session level if present for all
       out_buf += "c=IN IP6 ";
     out_buf += conn.address + "\r\n";
   }
-}
+//}
 
   out_buf += "t=0 0\r\n";
 
