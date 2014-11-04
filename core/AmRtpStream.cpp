1bc1c4b55be1aca0c5823af49a7726f405933eda
Michael Furmur <michael.f@didww.com>
2014-11-04 15:02:25 +0200

    AmRtpStream: add verbosity to message about RTP packet parsing error

--- core/AmRtpStream.cpp
+++ core/AmRtpStream.cpp
@@ -1019,7 +1019,7 @@ int AmRtpStream::nextPacket(AmRtpPacket*& p)
 	WARN("[%p] RTP Timeout detected. Last received packet is too old "
 	 "(diff.tv_sec = %i, limit = %i, "
 	 "remote_addr: %s:%i, "
-	 "local_ssrc: 0x%x, local_tag: %s]\n",
+	 "local_ssrc: 0x%x, local_tag: %s)\n",
 		 this,
 		 (unsigned int)diff.tv_sec,dead_rtp_time,
 		 get_addr_str(&r_saddr).c_str(),am_get_port(&r_saddr),
@@ -1091,7 +1091,11 @@ void AmRtpStream::recvPacket(int fd)
       parse_res = p->parse();
  
     if (parse_res == -1) {
-      DBG("error while parsing RTP packet.\n");
+	  ERROR("error while parsing RTP packet. "
+			"(remote_addr: %s:%i, "
+			"local_ssrc: 0x%x, local_tag: %s)\n",
+			get_addr_str(&r_saddr).c_str(),am_get_port(&r_saddr),
+			l_ssrc,session ? session->getLocalTag().c_str() : "no session");
       clearRTPTimeout(&p->recv_time);
 	  mem.freePacket(p);
     } else {
